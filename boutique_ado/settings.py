0accc4271a5b22f2349fc881c68e682bc115a2e9
mpysys <monnet.morgan@gmail.com>
2022-12-10 13:02:11 +0000

    deployment part two

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'profiles',
 
     'crispy_forms',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -177,6 +178,13 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+if 'USE_AWS' in os.environ:
+    # bucket config
+    AWS_STORAGE_BUCKET_NAME = 'boutique-ado-v1-momo'
+    AWS_S3_REGION_NAME = 'us-east-1'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY_ID = os.environ.get('AWS_SECRET_ACCESS_KEY_ID')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
